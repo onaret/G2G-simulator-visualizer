47616869b54146c13381ddad48947abd3578ab33
onaret <onaret@gmail.com>
2018-03-07 14:57:47 +0100

    UPD - specified route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/g2g-sim-visualizer', function(req, res, next) {
   res.render('index', { title: 'G2G - Manhattan plot visualizer' });
+});
+
+router.get('/g2g-sim-visual-izer', function(req, res, next) {
+  res.redirect('/g2g-sim-visualizer');
+});
+
+router.get('/', function(req, res, next) {
+  res.redirect('https://github.com/onaret');
 });
 
 module.exports = router;
